5b883b0e761708d6df827f8caf809f7d38c4e4d9
k-masters <masters.kaleb@gmail.com>
2018-12-07 15:02:55 -0500

    Working solution

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 freq_change_list = []
+base_freq = 0
 
-with open("input.txt", "r") as input_file:
+with open("Input Files/input.txt", "r") as input_file:
     for line in input_file:
-        freq_change_list.append(input_file.readline())
+        if line.endswith('\n'):
+            freq_change_list.append(int(line.rstrip('\n')))
+        else:
+            freq_change_list.append(int(line))
 
 print(freq_change_list)
+for freq_change in freq_change_list:
+    base_freq += freq_change
+
+print(base_freq)
