a731abd7dbcde90f87817a5a0b773af407ec7451
k-masters <masters.kaleb@gmail.com>
2018-12-07 15:21:55 -0500

    Add comments to day1-1.py

--- day1-1.py
+++ day1-1.py
@@ -1,6 +1,8 @@
+# initialize variables
 freq_change_list = []
 base_freq = 0
 
+# read file line by line adn strip off newline character if it is present, append the line to a list.
 with open("Input Files/input.txt", "r") as input_file:
     for line in input_file:
         if line.endswith('\n'):
@@ -8,7 +10,8 @@ with open("Input Files/input.txt", "r") as input_file:
         else:
             freq_change_list.append(int(line))
 
-print(freq_change_list)
+# print(freq_change_list)
+# iterate through list and apply changes to the base frequency
 for freq_change in freq_change_list:
     base_freq += freq_change
 
