bc4930380a3f946c91efcc3b0c0643282ce7a868
k-masters <masters.kaleb@gmail.com>
2018-12-10 06:34:08 -0500

    Add comments

--- day1-2.py
+++ day1-2.py
@@ -13,9 +13,13 @@ with open("Input Files/input.txt", "r") as input_file:
         else:
             freq_change_list.append(int(line))
 
+# we know we are going to go through the freq_change_list once, so the while statement evaluates to true at least once.
+# we then evaluate all freq changes and add the result to the freq history list and check to see if the list contains
+# the current freq result. If it does we have found a duplicate and the inner for and outer while loops can be
+# stopped. If we reach the end of the freq_change_list and a match still hasn't been found we start over from the
+# of the list.
 while not found_match:
     print("Looking for match on iteration: {}".format(str(iteration_num)))
-    iteration_num += 1
     for freq in freq_change_list:
         current_freq += freq
         if current_freq in freq_history:
@@ -25,3 +29,5 @@ while not found_match:
             break
         else:
             freq_history.append(current_freq)
+
+    iteration_num += 1
